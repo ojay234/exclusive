0c13661ac9cc66faf745b4191c98224eedc441af
David Ochuma <davidace998@gmail.com>
2024-11-26 14:04:28 +0100

    implemented user authentication

---
+++ api/controllers/auth.js
@@ -0,0 +1,52 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+
+// Register a new user
+const register = async (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res
+      .status(400)
+      .json({ message: "Username, email, and password are required" });
+  }
+
+  try {
+    const user = new User({ username, email, password });
+    await user.save();
+    res.json({ message: "Registration successful" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Login with an existing user
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const passwordMatch = await user.comparePassword(password);
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Incorrect password" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+      expiresIn: "1 hour",
+    });
+    res.json({ token });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { register, login };
